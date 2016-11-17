5ee8754abc6b5e2858c1afd2a6d3417321c3c5fd
Paul Kramme <pjkramme@gmail.com>
2016-11-17 20:15:18 +0100

    Remove unnecessary file creation

--- src/experimental/os_walk.py
+++ src/experimental/os_walk.py
@@ -11,8 +11,8 @@ print("Walk directory (absolute) = " + os.path.abspath(walk_dir))
 
 for root, subdirs, files in os.walk(walk_dir):
 	print("root = " + root)
-	list_file_path = os.path.join(root, "dirlist.txt")
-	print("dirlist = ", list_file_path)
+	#list_file_path = os.path.join(root, "dirlist.txt")
+	#print("dirlist = ", list_file_path)
 
 	#with open(list_file_path, 'wb') as list_file:
 	for subdir in subdirs:
