d7d4c9bf9f0bc7ed9701fa72270bfc13b1c2a013
Paul Kramme <pjkramme@gmail.com>
2016-11-20 17:07:46 +0100

    Add color support

--- src/btsoot.py
+++ src/btsoot.py
@@ -32,6 +32,17 @@ import sys
 import datatransfer-lib
 
 
+class color:
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+
+
 def usage():
 	print("Usage: btsoot command")
 	print("\tcreate config		| Creates configfile")
