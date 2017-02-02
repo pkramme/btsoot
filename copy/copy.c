097f1c545dfd4e15c122053c8d81695b906849c0
Paul Kramme <pjkramme@gmail.com>
2017-02-02 15:48:52 +0100

    Update copy.c

--- copy/copy.c
+++ copy/copy.c
@@ -1,3 +1,14 @@
+/*
+return -1 = incorrect number of args
+return 0 = ok
+return 1 = couldnt open source fd
+return 2 = couldnt open dest fd
+return 3 = couldnt write to dest fd
+return 4 = couldnt read from source fd
+return 5 = couldnt close source fd
+return 6 = couldnt close dest fd
+*/
+
 #include<stdio.h>
 #include<fcntl.h>
 #include<sys/stat.h>
@@ -8,17 +19,6 @@ int copy(char *source, char *destination);
 int main(int argc, char *argv[])
 {
 	int exit_code;
-	
-	/*
-	return -1 = incorrect number of args
-	return 0 = ok
-	return 1 = couldnt open source fd
-	return 2 = couldnt open dest fd
-	return 3 = couldnt write to dest fd
-	return 4 = couldnt read from source fd
-	return 5 = couldnt close source fd
-	return 6 = couldnt close dest fd
-	*/
 
 	if(argc != 3)
 	{
