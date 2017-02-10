94dd837179ebeba37206f6aa50f88df311714e93
Paul Kramme <pjkramme@gmail.com>
2017-02-10 23:33:42 +0100

    Add version replacement tool

---
+++ package_deb_replace_version.py
@@ -0,0 +1,18 @@
+import sys
+path = "./btsoot/DEBIAN/control"
+version = sys.argv[2]
+control_content = f"""Package: btsoot
+Version: {version}
+Section: base
+Priority: optional
+Architecture: i386
+Maintainer: Paul Kramme <pjkramme@gmail.com>
+Description: BTSOOT
+ Folder redundancy offsite-backup utility.
+"""
+print("DEB PACKAGE VERSION REPLACER")
+# yes, i wrote a tool for this...
+with open(path, "w") as f:
+	f.write(control_content)
+print("Done.")
+
