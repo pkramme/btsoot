a34ff47a3bdc5911403017f3fddcf03f024d2927
Paul Kramme <pjkramme@gmail.com>
2017-02-11 00:43:56 +0100

    Update package_deb_replace_version.py

--- package_deb_replace_version.py
+++ package_deb_replace_version.py
@@ -1,6 +1,20 @@
 import sys
-version = sys.argv[1]
-path = f"btsoot_{version}/DEBIAN/control"
+
+def split(string, splitters): #MAY RESOLVE ALL PROBLEMS WITH CSV
+	final = [string]
+	for x in splitters:
+    	for i,s in enumerate(final):
+			if x in s and x != s:
+				left, right = s.split(x, 1)
+				final[i] = left
+				final.insert(i + 1, x)
+				final.insert(i + 2, right)
+	return final
+
+path = "btsoot/DEBIAN/control"
+fullversion = sys.argv[1]
+version = split(fullversion, "v")
+version = version[1]
 control_content = f"""Package: btsoot
 Version: {version}
 Section: base
@@ -16,4 +30,3 @@ print("DEB PACKAGE VERSION REPLACER")
 with open(path, "a") as f:
 	f.write(control_content)
 print("Done.")
-
