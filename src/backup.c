0c7292c5a86e67c6172e9e02bf1b217c7aabccae
Paul Kramme <pjkramme@gmail.com>
2017-02-14 20:57:04 +0100

    Add backup c files

---
+++ src/backup.c
@@ -0,0 +1,14 @@
+#include"backup.h"
+
+int backup(void)
+{
+	/* BACKUP PIPELINE
+	 * 
+	 * Needed functions:
+	 *  - scan for files and directories, record size of files
+	 *  - scan for crc values
+	 *  - diff this scan with the last
+	 *  - execute all necessary changes
+	 */
+	return 0;
+}
