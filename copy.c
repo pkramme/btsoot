a6cac0b6477abaa0c3caa60295c170b240164636
Paul Kramme <pjkramme@gmail.com>
2017-03-05 23:14:47 +0100

    Update copy.c

--- copy.c
+++ copy.c
@@ -6,8 +6,8 @@ return -1 = incorrect number of args
 return 0 = ok
 return 1 = couldnt open source fd
 return 2 = couldnt open dest fd
-return 3 = couldnt write to dest fd
-return 4 = couldnt read from source fd
+return 3 = couldnt write to dest fd - NOT EXISTENT IN COPY()
+return 4 = couldnt read from source fd - NOT EXISTENT IN COPY()
 return 5 = couldnt close source fd
 return 6 = couldnt close dest fd
 */
