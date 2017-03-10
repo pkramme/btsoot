d1d0b62c3266c4afb565e46d2fc6425474d38e60
Paul Kramme <pjkramme@gmail.com>
2017-03-11 00:42:26 +0100

    Add license

--- btsoot.c
+++ btsoot.c
@@ -111,7 +111,25 @@ int main(int argc, char *argv[])
 	{
 		if(strcmp("license", argv[1]) == 0)
 		{
-			puts("BTSOOT is licensed under MIT License.");
+			puts("BTSOOT\n");
+			puts("MIT License\n");
+			puts("Copyright (c) 2016 Paul Kramme\n");
+			puts("Permission is hereby granted, free of charge, to any person obtaining a copy");
+			puts("of this software and associated documentation files (the \"Software\"), to deal");
+			puts("in the Software without restriction, including without limitation the rights");
+			puts("to use, copy, modify, merge, publish, distribute, sublicense, and/or sell");
+			puts("copies of the Software, and to permit persons to whom the Software is");
+			puts("furnished to do so, subject to the following conditions:\n");
+			puts("The above copyright notice and this permission notice shall be included in all");
+			puts("copies or substantial portions of the Software.\n");
+			puts("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR");
+			puts("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,");
+			puts("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE");
+			puts("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER");
+			puts("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,");
+			puts("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE");
+			puts("SOFTWARE.");
+
 			puts("\nBelow are licenses for dependencies\n");
 
 			puts("xxHash Library");
