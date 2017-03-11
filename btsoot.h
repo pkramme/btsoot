7ad25bbc2a0c40defcfc4aaf5c6d46d3e7f90920
Paul Kramme <pjkramme@gmail.com>
2017-03-11 20:58:32 +0100

    Update btsoot.h

--- btsoot.h
+++ btsoot.h
@@ -5,14 +5,12 @@
 #include<string.h>
 
 #include"color.h"
+#include"config.h"
 
 #define MAX_PATH_LEN 4096
 
 #define PIP_PURP_ID_BACKUP 1
 #define PIP_PURP_ID_RESTORE 2
-
-#define CONFIG_PATH "btsoot.conf"
-#define COPY_CONFIG_PATH "btsoot.conf.temp"
 
 struct job {
 	int pip_purp_id;
