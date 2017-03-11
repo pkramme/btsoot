4c88d6628aa7ba4b2b14d40a17813215033f109d
Paul Kramme <pjkramme@gmail.com>
2017-03-11 20:57:52 +0100

    Update config.h

--- config.h
+++ config.h
@@ -1,3 +1,5 @@
 #pragma once
 
-#define database_path "./"
+#define database_path "./"  //Storage path for all database files.
+#define CONFIG_PATH "btsoot.conf"   //Storage path for config file.
+#define COPY_CONFIG_PATH "btsoot.conf.temp" //Storage path for temporary config file.
