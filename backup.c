5e04e37be9d9d98e1bfb37f38ad189964a6a5499
Paul Kramme <pjkramme@gmail.com>
2017-03-15 21:05:07 +0100

    Add theoretical threading model

--- backup.c
+++ backup.c
@@ -1,9 +1,15 @@
 #include"backup.h"
 
+#define MAX_THREADS 4
+
 static sqlite3 *database = NULL;
 
 time_t t0;
 time_t tsearched;
+size_t total_size = 0;
+size_t max_thread_size = 0;
+size_t curr_size = 0; //Stores current size for thread filling
+int thread_number = 0;
 
 static int filewalk_info_callback(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf)
 {
@@ -21,6 +27,7 @@ static int filewalk_info_callback(const char *fpath, const struct stat *sb, int 
 		case FTW_F:
 		{
 			size_t initsize;
+			total_size += sb->st_size;
 			if(sb->st_size < FILEBUFFER) 
 			{
 				initsize = sb->st_size;
@@ -67,7 +74,7 @@ static int filewalk_info_callback(const char *fpath, const struct stat *sb, int 
 	return 0;
 }
 
-static int diff_callback(void *notused, int argc, char **argv, char **azcolname)
+static int time_callback(void *notused, int argc, char **argv, char **azcolname)
 {
 	for(int i = 0; i < argc; i++)
 	{
@@ -80,9 +87,33 @@ static int diff_callback(void *notused, int argc, char **argv, char **azcolname)
 	return 0;
 }
 
+static int sql_hash(void *notused, int argc, char **argv, char **azcolname)
+{
+	for(int i = 0; i < argc; i++)
+	{		
+		printf("%s = %s\n", azcolname[i], argv[i] ? argv[i] : "NULL");
+		if(strcmp(azcolname[i], "size") == 0)
+		{
+			if(curr_size <= max_thread_size)
+			{
+				curr_size += atoi(argv[i]);
+				char *zsql = sqlite3_mprintf(
+					"UPDATE files SET thread = %i WHERE path = '%s'", thread_number, argv[i - 1]);
+			}
+			else
+			{
+				curr_size = 0;
+				thread_number += 1;
+			}
+		}
+	}
+	return 0;
+}
+
 int backup(job_t *job_import)
 {
 	t0 = time(0);
+	char *errormessage = 0;
 
 	db_init(job_import->db_path);	//create and open database
 	sqlite3_open(job_import->db_path, &database);
@@ -100,20 +131,22 @@ int backup(job_t *job_import)
 
 	puts("Searching for previous scan");
 	int *timestamp = NULL;
-	char *errormesgselect = 0;
 	char *zsqlsel = sqlite3_mprintf("SELECT * FROM files WHERE %i > scantime ORDER BY scantime DESC", t0);
-	sqlite3_exec(database, zsqlsel, diff_callback, timestamp, &errormesgselect);
-	if(errormesgselect != NULL)
+	sqlite3_exec(database, zsqlsel, time_callback, NULL, &errormessage);
+	if(errormessage != NULL)
 	{
-		if(strcmp(errormesgselect, "callback requested query abort") != 0)
+		if(strcmp(errormessage, "callback requested query abort") != 0)
 		{
-			printf("%s\n", errormesgselect);
+			printf("%s\n", errormessage);
 		}
 	}
 	sqlite3_free(zsqlsel);
 
+	max_thread_size = total_size / MAX_THREADS;
 
-
+	char *zsqlhash = sqlite3_mprintf("SELECT path, size FROM files WHERE scantime = %i AND type = 0", tsearched);
+	sqlite3_exec(database, zsqlhash, sql_hash, NULL, &errormessage);
+	printf("%li", total_size);
 
 
 	sqlite3_exec(database, "END TRANSACTION", NULL, NULL, NULL);
