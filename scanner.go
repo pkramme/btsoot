7233e729173b429438c094ae86dc19545679ef07
Paul Kramme <pjkramme@gmail.com>
2017-05-22 15:24:10 +0200

    Add basic scanner code from media validation tool This adds sha256sum calculation and a simple recursive file scanner

---
+++ scanner.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func sha256sum(filePath string) (result string, err error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	hash := sha256.New()
+	_, err = io.Copy(hash, file)
+	if err != nil {
+		return
+	}
+	result = hex.EncodeToString(hash.Sum(nil))
+	return
+}
+
+func scanfiles(location string) (m map[string]string, err error) {
+	m = make(map[string]string)
+	var walkcallback = func(path string, fileinfo os.FileInfo, inputerror error) (err error) {
+		checksum, _ := sha256sum(path)
+		m[path] = checksum
+		return
+	}
+	err = filepath.Walk(location, walkcallback)
+	return
+}
