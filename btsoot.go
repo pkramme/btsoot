e962874dd3f426b586149dda03b273ddd6048102
Paul Kramme <pjkramme@gmail.com>
2017-07-09 23:54:21 +0200

    Add cli code

--- btsoot.go
+++ btsoot.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"github.com/paulkramme/cli"
 	"github.com/paulkramme/ini"
 	"log"
 	"os"
@@ -16,39 +16,73 @@ const (
 )
 
 func main() {
-	fmt.Println("BTSOOT - Copyright (c) 2017 Paul Kramme All Rights Reserved.")
+	app := cli.NewApp()
 
-	ConfigLocation := flag.String("c", "", "Specifies configfile location")
-	flag.Parse()
-
-	Config := new(Configuration)
-
-	err := ini.MapTo(Config, *ConfigLocation)
-	if err != nil {
-		panic(err)
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "config, c",
+			Usage:       "Specifies the location of the configfile",
+		},
+		cli.StringFlag{
+			Name:  "lang",
+			Value: "english",
+			Usage: "language for the greeting",
+		},
 	}
 
-	var f *os.File
-	if Config.LogFileLocation != "" {
-		f, err = os.OpenFile(Config.LogFileLocation, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		defer f.Close()
-		log.SetOutput(f)
-	}
+	app.Commands = []cli.Command{
+		{
+			Name:  "init",
+			Usage: "Initialize a new block",
+			Action: func(c *cli.Context) {
+				// Init(c.String("config"))
+				fmt.Println("init")
+				fmt.Println(c.String("lang"))
+			},
+		},
+		{
+			Name:  "license",
+			Usage: "Show all licenses associated with this project",
+			Action: func(c *cli.Context) {
+				License()
+			},
+		},
+		{
+			Name:  "backup",
+			Usage: "Backups the dataset",
+			Action: func(c *cli.Context) {
+				Config := new(Configuration)
 
-	log.Println("BTSOOT started")
+				err := ini.MapTo(Config, c.String("config"))
+				if err != nil {
+					panic(err)
+				}
 
-	Data := new(Block)
-	err = Load(Config.DBFileLocation, Data)
-	if err != nil {
-		fmt.Println("Datafile not found. Please initialize the file")
-	}
+				var f *os.File
+				if Config.LogFileLocation != "" {
+					f, err = os.OpenFile(Config.LogFileLocation, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+					if err != nil {
+						log.Fatalln(err)
+					}
+					defer f.Close()
+					log.SetOutput(f)
+				}
 
-	Scan.Scans[time.Now().Format(time.RFC3339)] = ScanFiles(Config.Source, Config.MaxWorkerThreads)
-	err = Save(Config.DBFileLocation, NewScan)
-	if err != nil {
-		panic(err)
+				log.Println("BTSOOT started")
+
+				Data := new(Block)
+				err = Load(Config.DBFileLocation, Data)
+				if err != nil {
+					fmt.Println("Datafile not found. Please initialize the file")
+				}
+
+				Data.Scans[time.Now().Format(time.RFC3339)] = ScanFiles(Config.Source, Config.MaxWorkerThreads)
+				err = Save(Config.DBFileLocation, Data)
+				if err != nil {
+					panic(err)
+				}
+			},
+		},
 	}
+	app.Run(os.Args)
 }
