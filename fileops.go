f8893cb185e9f98b09726be3592a58aa2167f70e
Paul Kramme <pjkramme@gmail.com>
2017-07-24 14:42:20 +0200

    Change main fileformat to json

--- fileops.go
+++ fileops.go
@@ -1,32 +1,30 @@
 package main
 
 import (
-	"encoding/gob"
+	"encoding/json"
 	"fmt"
-	"os"
+	"io/ioutil"
 )
 
-// Save saves a datastructure to a specified path with gob encoding.
+// Save saves a datastructure to a specified path with json encoding.
 func Save(path string, object interface{}) error {
-	file, err := os.Create(path)
-	if err == nil {
-		encoder := gob.NewEncoder(file)
-		err := encoder.Encode(object)
-		if err != nil {
-			fmt.Println(err)
-		}
+	encodedobject, err := json.MarshalIndent(object, "", "\t")
+	if err != nil {
+		fmt.Println(err)
 	}
-	file.Close()
+	err = ioutil.WriteFile(path, encodedobject, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
 	return err
 }
 
-// Load loads gob data from a file into a datastructure.
+// Load loads json data from a file into a datastructure.
 func Load(path string, object interface{}) error {
-	file, err := os.Open(path)
-	if err == nil {
-		decoder := gob.NewDecoder(file)
-		err = decoder.Decode(object)
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Println(err)
 	}
-	file.Close()
+	json.Unmarshal(file, object)
 	return err
 }
